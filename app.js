a60569d55cd4b5cbd15113a9bf5f927aa1b5132a
Caleb Hagood <calebhagood99@gmail.com>
2023-03-13 02:13:18 -0400

    added change mode feature with click, and also added an error message for invalid input
    will update css next

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let color = "black";
-
+let click = true;
 
 function makeGrid(size){
     let grid = document.querySelector('.grid');
@@ -21,19 +21,22 @@ makeGrid(16);
 
 function changeSize(size){
     if(size >= 2 && size <= 100){
+        document.querySelector('.error').style.display = 'none';
         makeGrid(size);
     }
     else {
-        console.log("Grid too large or too small");
+        document.querySelector('.error').style.display = 'flex';
     }
 }
 
 function colorSquare(){
-    if(color == 'random'){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    }
-    else{
-        this.style.backgroundColor = color;
+    if(click){
+        if(color == 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+        else{
+            this.style.backgroundColor = color;
+        }
     }
 }
 
@@ -43,6 +46,19 @@ function changeColor(colorChoice){
 
 function resetGrid(){
     let grid = document.querySelector('.grid');
-    let squares = grid.querySelectorAll('div')
+    let squares = grid.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = 'white');
 }
+
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if(e.target.tagName != "BUTTON"){
+        click = !click;
+    if(click){
+        document.querySelector('.mode').textContent = "Mode: Drawing";
+    }
+    else{
+        document.querySelector('.mode').textContent = "Mode: Not Drawing";
+    }
+    }
+})
