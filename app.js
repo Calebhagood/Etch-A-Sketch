ddfe27c50d9d592300d70ef81b35ae205348ad1e
Caleb Hagood <calebhagood99@gmail.com>
2023-03-15 14:09:04 -0400

    Updated the styles.css file to create a better looking UI for the user

--- app.js
+++ app.js
@@ -1,64 +1,56 @@
 let color = "black";
 let click = true;
+let size = 16;
 
-function makeGrid(size){
-    let grid = document.querySelector('.grid');
-    let squares = grid.querySelectorAll('div')
-    squares.forEach((div) => div.remove());
-    grid.style.gridTemplateColumns =  `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+function makeGrid(size) {
+  let grid = document.querySelector(".grid");
+  let squares = grid.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    let amount = size * size;
-    for (let i=0; i<amount; i++){
-        let square = document.createElement('div');
-        square.addEventListener("mouseover", colorSquare)
-        square.style.backgroundColor = "white";
-        grid.insertAdjacentElement('beforeend', square);
-    }
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    grid.insertAdjacentElement("beforeend", square);
+  }
 }
 
 makeGrid(16);
 
-function changeSize(size){
-    if(size >= 2 && size <= 100){
-        document.querySelector('.error').style.display = 'none';
-        makeGrid(size);
+function changeSize(size) {
+  makeGrid(size);
+}
+
+function colorSquare() {
+  if (click) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
     }
-    else {
-        document.querySelector('.error').style.display = 'flex';
-    }
+  }
 }
 
-function colorSquare(){
-    if(click){
-        if(color == 'random'){
-            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-        }
-        else{
-            this.style.backgroundColor = color;
-        }
-    }
+function changeColor(colorChoice) {
+  color = colorChoice;
 }
 
-function changeColor(colorChoice){
-    color = colorChoice;
+function resetGrid() {
+  let grid = document.querySelector(".grid");
+  let squares = grid.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
-
-function resetGrid(){
-    let grid = document.querySelector('.grid');
-    let squares = grid.querySelectorAll('div');
-    squares.forEach((div) => div.style.backgroundColor = 'white');
-}
-
 
 document.querySelector("body").addEventListener("click", (e) => {
-    if(e.target.tagName != "BUTTON"){
-        click = !click;
-    if(click){
-        document.querySelector('.mode').textContent = "Mode: Drawing";
+  if (e.target.tagName != "BUTTON") {
+    click = !click;
+    if (click) {
+      document.querySelector(".mode").textContent = "Mode: Drawing";
+    } else {
+      document.querySelector(".mode").textContent = "Mode: Not Drawing";
     }
-    else{
-        document.querySelector('.mode').textContent = "Mode: Not Drawing";
-    }
-    }
-})
+  }
+});
